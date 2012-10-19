8776a0ab6bf7f19f6a6e40483285dafc27a6242d
foxcarlos <foxcarlos@gmail.com>
2012-10-19 14:33:46 -0430

    modified:   sensor_pir/sensor_pir.ino

--- sensor_pir/sensor_pir.ino
+++ sensor_pir/sensor_pir.ino
@@ -24,12 +24,12 @@ void loop() {
   int pirVal = digitalRead(pirpin);
   
   if(pirVal == 1){
-    Serial.println(pirVal);
+    Serial.println("Movimiento");
     digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
     //delay(100);               // wait for a second
   }
   else {
-    Serial.println(pirVal);
+    Serial.println("Nadie");
     digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   }
 }
